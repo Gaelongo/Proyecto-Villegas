64950ce87b6e84c4d8287e0ba255908a8c1463f4
Mario Aguilar Real <94491813+MarioAguilarReal@users.noreply.github.com>
2023-05-28 18:32:37 -0700

    Trabajo iniciarl en la navegación del proyecto

--- ProyectoFinal/src/proyectofinal/ProyectoFinal.java
+++ ProyectoFinal/src/proyectofinal/ProyectoFinal.java
@@ -3,10 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package proyectofinal;
-
+import java.util.Scanner;
 /**
  *
- * @author Gael
+ * @author Gael Mario Fernanda
  */
 public class ProyectoFinal {
 
@@ -14,7 +14,14 @@ public class ProyectoFinal {
      * @param args the command line arguments
      */
     
-    
+    public void menu(){
+        Scanner leer = new Scanner(System.in);
+        int op = 0;
+
+        do{
+            System.out.println
+        }
+    }
     
     public static void main(String[] args) {
         
